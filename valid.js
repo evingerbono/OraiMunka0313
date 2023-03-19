5c1beae214972ccab116b8fccac4c56411705ab2
evingerbono <bono0823@gmail.com>
2023-03-19 13:08:41 +0100

    Jelszó_egyezés

--- valid.js
+++ valid.js
@@ -1,9 +1,17 @@
 function validateForm() {
-
     let x = document.validTeszt.knev.value;
-    if (x === "") {
+    let y = document.validTeszt.jelszo1.value;
+    let z = document.validTeszt.jelszo2.value;
+    
+    if (x === "" || y === "" || z === "") {
         alert("A mező kitöltése kötelező!");
         return false;
     }
+    
+    if (y !== z) {
+        alert("A két jelszó nem egyezik meg!");
+        return false;
+    }
+    
     return true;
 }
